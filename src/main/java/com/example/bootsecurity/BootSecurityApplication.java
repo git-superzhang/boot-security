93fc145b21bffc3028cbe819aaacf56d15e7931e
superzhang <go.superzhang@gmail.com>
2022-05-13 11:39:56 +0800

    hot-fix commit2

--- src/main/java/com/example/bootsecurity/BootSecurityApplication.java
+++ src/main/java/com/example/bootsecurity/BootSecurityApplication.java
@@ -20,6 +20,7 @@ public class BootSecurityApplication {
 
         logger.info("test commit");
         logger.info("hot-fix commit");
+        logger.info("hot-fix commit2");
 
         app.run(args);
 
